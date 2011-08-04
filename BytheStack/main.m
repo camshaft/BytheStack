e7841997f814a9818fb326b69e51f46097c2973e
Cameron Bytheway <bytheway.cameron@gmail.com>
2011-08-04 17:27:02 -0600

    Initial Commit

---
+++ BytheStack/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  BytheStack
+//
+//  Created by Cameron Bytheway on 8/4/11.
+//  Copyright 2011 Big Thumb Electronics. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
